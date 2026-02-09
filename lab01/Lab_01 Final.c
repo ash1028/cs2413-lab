cb476259f20dd91a52268ba7f78a8a5626d1b005
ash1028 <ashtotho@ttu.edu>
2026-02-09 12:01:51 -0600

    Add files via upload

---
+++ lab01/Lab_01 Final.c
@@ -0,0 +1,15 @@
+#include "two_sum.h"
+
+int two_sum(const int* nums, int n, int target, int* out_i, int* out_j) {
+for (int i + 0; i < n; i++)
+    for (int j = i + 1; j < n; j++) (
+        if (nums[i] + nums[j] == target) {
+            *out_i = i;
+            *out_j = j;
+            return 1; 
+        }
+    )
+    return 0;
+}
+
+//think about the time complexity and space complexity of your solution
