6364092c0fe9fcd95d1f5f1aba09633da13882c5
ash1028 <ashtotho@ttu.edu>
2026-03-23 23:23:40 -0500

    Delete lab04/LAB04 Question1 FINAL.c

--- lab04/LAB04 Question1 FINAL.c
+++
@@ -1,7 +0,0 @@
-#include <stddef.h>
-int maxDepth(struct TreeNode* root) {
-    if (!root) return 0;
-    int left = maxDepth(root->left);
-    int right = maxDepth(root->right);
-    return (left > right ? left : right) + 1;
-}
